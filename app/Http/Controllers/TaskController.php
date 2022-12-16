586c37b5c002ae7bccd8fcecdcb05a6ef70cf2b9
dawidbros1 <dawidbros1@wp.pl>
2022-12-16 12:01:46 +0100

    added data validation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,37 +3,36 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\StoreRequest;
 use App\Models\Task;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     // Metoda do dodawania kategorii
-    public function store(Request $request, $category)
+    public function store(StoreRequest $request, $category)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|min:6|max:255',
-            'description' => 'required',
+        $request->validated();
+
+        Task::created([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'deadline' => $request->input('deadline'),
+            'category_id' => $category,
         ]);
-
-        $task = new Task();
-        $task->name = $request->name;
-        $task->description = $request->description;
-        $task->deadline = $request->input('deadline');
-        $task->category_id = $category; // id
-
-        $task->save();
 
         return back()->withSuccess('Zadanie zostało dodane');
     }
 
-    public function update(Request $request, Task $task)
+    public function update(StoreRequest $request, Task $task)
     {
-        $task->name = $request->input('name');
-        $task->description = $request->input('description');
-        $task->deadline = $request->input('deadline');
-        $task->save();
+        $request->validated();
+
+        $task->update([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'deadline' => $request->input('deadline'),
+        ]);
 
         return redirect()->route('category.show', $task->category)->withSuccess("Dane zostały zaktualizowane");
     }
@@ -53,9 +52,14 @@ class TaskController extends Controller
 
     public function finish(Task $task)
     {
-        $task->status = "finished";
-        $task->finished_at = Carbon::now('Europe/Warsaw');
-        $task->save();
+        $task->update([
+            'status' => 'finished',
+            'finished_at' => Carbon::now('Europe/Warsaw'),
+        ]);
+
         return back()->withSuccess("Zadanie zostało ukończone");
     }
 }
+
+# Zadań po terminiu nie można edytować oraz usuwać => tylko zakonczyć
+# Czy w update ma być możliwość zmiany terminu ?
