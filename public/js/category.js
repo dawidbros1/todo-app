f52f6141394d0c65909438b6d991502c6b006ce9
dawidbros1 <dawidbros1@wp.pl>
2022-12-16 11:07:07 +0100

    deleted script which open edited form and fill itdeleted script which open edit form and fill it

--- public/js/category.js
+++ public/js/category.js
@@ -11,11 +11,11 @@ var list = document.getElementById('category-list');
 var _loop = function _loop(i) {
   handles[i].addEventListener('click', function () {
     closeWrappers();
-    list.classList.add('blue');
+    list.classList.add('blur');
     wrappers[i].classList.remove('d-none');
   });
   closeHandles[i].addEventListener('click', function () {
-    list.classList.remove('blue');
+    list.classList.remove('blur');
     wrappers[i].classList.add('d-none');
   });
 };
@@ -25,21 +25,6 @@ for (var i = 0; i < handles.length; i++) {
 function closeWrappers() {
   for (var _i = 0; _i < wrappers.length; _i++) {
     wrappers[_i].classList.add('d-none');
-  }
-}
-var url = window.location.href;
-var parts = url.split("#");
-if (parts.length != 1) {
-  var id = parts[1];
-  var value = parts[2];
-  if (id != 0) {
-    for (var _i2 = 0; _i2 < wrappers.length; _i2++) {
-      if (wrappers[_i2].dataset.id == id) {
-        handles[_i2].click();
-        wrappers[_i2].querySelector('input.input-name').value = value.replace(/%20/g, ' ');
-        ;
-      }
-    }
   }
 }
 /******/ })()
--- resources/js/category.js
+++ resources/js/category.js
@@ -5,39 +5,21 @@ var closeHandles = document.getElementsByClassName('close-edit-form-handle')
 var list = document.getElementById('category-list');
 
 for (let i = 0; i < handles.length; i++) {
-   handles[i].addEventListener('click', () => {
-      closeWrappers();
-      list.classList.add('blue')
-      wrappers[i].classList.remove('d-none');
-   })
+    handles[i].addEventListener('click', () => {
+        closeWrappers();
+        list.classList.add('blur')
+        wrappers[i].classList.remove('d-none');
+    })
 
-   closeHandles[i].addEventListener('click', () => {
-      list.classList.remove('blue')
-      wrappers[i].classList.add('d-none')
-   })
+    closeHandles[i].addEventListener('click', () => {
+        list.classList.remove('blur')
+        wrappers[i].classList.add('d-none')
+    })
 }
 
 function closeWrappers() {
-   for (let i = 0; i < wrappers.length; i++) {
-      wrappers[i].classList.add('d-none');
-   }
+    for (let i = 0; i < wrappers.length; i++) {
+        wrappers[i].classList.add('d-none');
+    }
 }
-
-const url = window.location.href;
-const parts = url.split("#");
-
-if (parts.length != 1) {
-   const id = parts[1]
-   const value = parts[2]
-
-   if (id != 0) {
-      for (let i = 0; i < wrappers.length; i++) {
-         if (wrappers[i].dataset.id == id) {
-            handles[i].click();
-            wrappers[i].querySelector('input.input-name').value = value.replace(/%20/g, ' ');;
-         }
-      }
-   }
-}
-
 
